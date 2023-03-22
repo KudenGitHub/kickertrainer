4f35a252888f625ed199c3bd13a63b7490e75caa
Dennis Kubousek <dennis.kubousek@gmx.de>
2023-03-22 12:50:09 +0100

    Initial Commit

---
+++ Kickertrainer/KickertrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KickertrainerApp.swift
+//  Kickertrainer
+//
+//  Created by Dennis Kubousek on 22.03.23.
+//
+
+import SwiftUI
+
+@main
+struct KickertrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
